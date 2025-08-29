abbb1bcf2aa14e0fdc7d281eeaedac9351446924
Jonathan Gilreath <jhgilre@g.clemson.edu>
2025-08-29 16:44:53 -0400

    Enhanced Comments

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -28,7 +28,7 @@ class QuoteCard extends StatelessWidget {
               quote.author,
               style: TextStyle(fontSize: 14.0, color: Colors.grey[800]),
             ),
-            //Like button
+            //Date & Like button
             SizedBox(height: 6.0),
             Row(
               mainAxisSize: MainAxisSize.min,
